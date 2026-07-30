f7a6742cf3df4cafff5f7bf38215a6754689766e
MohammadRefat23 <39741968+MohammadRefat23@users.noreply.github.com>
2026-07-30 21:22:29 +0000

    Automatically update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,16 +1,11 @@
 var addressPoints = [
   [
-    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />Simons Foundation; New York City, NY, USA",
-    40.7127281,
-    -74.0060152
-  ],
-  [
     "Starspot Inference with Light Curve Inversion Techniques<br />Center for Computational Astrophysics; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
   [
-    "Two-Point Statistics in the Star Forming ISM<br />Simons Foundation; New York City, NY, USA",
+    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />Simons Foundation; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
@@ -18,6 +13,11 @@ var addressPoints = [
     "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />AAS 240; Pasadena, CA, USA",
     34.1476507,
     -118.144155
+  ],
+  [
+    "Two-Point Statistics in the Star Forming ISM<br />Simons Foundation; New York City, NY, USA",
+    40.7127281,
+    -74.0060152
   ],
   [
     "Where Were Stars Born in The Milky Way?<br />American Museum of Natural History; New York City, NY, USA",
