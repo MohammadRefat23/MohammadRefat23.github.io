e2746f22f826a90b99ace30461c8baec0a842ec4
MohammadRefat23 <39741968+MohammadRefat23@users.noreply.github.com>
2026-08-02 09:07:13 +0000

    Automatically update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,31 +1,31 @@
 var addressPoints = [
   [
-    "Starspot Inference with Light Curve Inversion Techniques<br />Center for Computational Astrophysics; New York City, NY, USA",
+    "Starspot Inference with Light Curve Inversion Techniques<br />Flatiron Institute, Center for Computational Astrophysics; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
   [
-    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />Simons Foundation; New York City, NY, USA",
+    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />Flatiron Institute, Center for Computational Astrophysics; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
   [
-    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />AAS 240; Pasadena, CA, USA",
+    "Towards Mapping Brown Dwarf and Giant Exoplanet Atmospheres<br />American Astronomical Society; Pasadena, CA, USA",
     34.1476507,
     -118.144155
   ],
   [
-    "Two-Point Statistics in the Star Forming ISM<br />Simons Foundation; New York City, NY, USA",
+    "Two-Point Statistics in the Star-Forming ISM<br />Flatiron Institute, Center for Computational Astrophysics; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
   [
-    "Where Were Stars Born in The Milky Way?<br />American Museum of Natural History; New York City, NY, USA",
+    "Where Were Stars Born in the Milky Way?<br />American Museum of Natural History; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ],
   [
-    "Chemodynamically Characterizing the Jhelum Stellar Stream with APOGEE-2<br />Simons Foundation; New York City, NY, USA",
+    "Chemodynamically Characterizing the Jhelum Stellar Stream with APOGEE-2<br />American Museum of Natural History; New York City, NY, USA",
     40.7127281,
     -74.0060152
   ]
